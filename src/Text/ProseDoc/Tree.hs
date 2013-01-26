3d1e7dcb2c37e2d59506f3212484f125ae7afa7e
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2013-01-26 21:46:59 +0200

    Implemented a splitTree function
    We need to be able to split the presentation tree into sections
    when we run into ProseComment labels.
    
    The splitTree function is a generic function that walks down the
    tree until it finds a label that fulfils a test condition. It
    breaks the tree in three parts at that point: left, separator and
    right. The separator is the tree that starts from the separating
    label and left and right are the half-trees on either side.
    
    The spine of the tree all the way to the separation point is
    duplicated for both left and right half-trees.

--- src/Text/ProseDoc/Tree.hs
+++ src/Text/ProseDoc/Tree.hs
@@ -31,16 +31,60 @@ foldTree bf lf = go where
     go (Leaf t)        = lf t
 
 {-%
-Trees form a monoid. The label type is constrained to `Eq` so that we can
-directly merge tree with an identical label.
+Trees form a monoid so that the two trees are merged into the same branch.
 -}
-instance Eq l => Monoid (Tree l n) where
+instance Monoid (Tree l n) where
     mempty          = Empty
 
     mappend Empty b = b
     mappend a Empty = a
-    mappend (Label l t) (Label l' t') | l == l' = Label l (t <> t')
     mappend (Branch b) (Branch b') = Branch (b <> b')
     mappend a (Branch b) = Branch (a:b)
     mappend a b = Branch [a,b]
 
+{-%
+A tree can be split into parts based a label test. The split off parts will
+contain the same labels when walking the tree spine as the part they were cut
+off from.
+-}
+splitTree :: (l -> Bool) -> Tree l n -> (Tree l n, Maybe (Tree l n), Tree l n)
+splitTree test = go where
+    go t = case t of
+        Empty -> (Empty, Nothing, Empty)
+        Label l t'
+            | test l    -> (Empty, Just (Label l t'), Empty)
+            | otherwise -> (Label l a, sep, b')
+            where
+                (a, sep, b) = go t'
+                b' = case sep of
+                    Nothing -> Empty
+                    _       -> Label l b
+        Leaf n    -> (Leaf n, Nothing, Empty)
+        Branch bs -> case sep of
+            Nothing -> (Branch bs, Nothing, Empty)
+            _       -> (mconcat a, sep, mconcat b)
+            where (a, sep, b) = gob bs
+
+    gob [] = ([], Nothing, [])
+    gob (b:bs) = case sep of
+        Nothing -> (l:b', sep', bs')
+        _       -> (l:[], sep, r:bs)
+        where
+            (l, sep, r) = go b
+            (b', sep', bs') = gob bs
+
+
+testSplit = splitTree (=='e') t where
+    t = Branch [b1,b2,b3]
+    b1 = Label '1' $ Branch [a,b,c]
+    a = Label 'a' (Leaf 1)
+    b = Label 'b' (Leaf 2)
+    c = Label 'c' (Leaf 3)
+    b2 = Label '2' $ Branch [d,e,f]
+    d = Label 'd' (Leaf 4)
+    e = Label 'e' (Leaf 5)
+    f = Label 'f' (Leaf 6)
+    b3 = Label '3' $ Branch [g,h,i]
+    g = Label 'g' (Leaf 7)
+    h = Label 'h' (Leaf 8)
+    i = Label 'i' (Leaf 9)
